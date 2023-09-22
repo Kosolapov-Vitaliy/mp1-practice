091f02bd50b40bb8d8c4d42c24b0c8f208153e44
Kosolapov-Vitaliy <givit3324@gmail.com>
2023-09-22 14:03:26 +0300

    Fix programm

--- MFirstSolution/MFirstProject/Main.c
+++ MFirstSolution/MFirstProject/Main.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf("Hello, World!");
+    int a1, b1;
+    printf("Input a:");
+    scanf("%d", &a1);
+    printf("Input b:");
+    scanf("%d", &b1);
+    printf("a+b= %d\n", a1 + b1);
     return 0;
 }
