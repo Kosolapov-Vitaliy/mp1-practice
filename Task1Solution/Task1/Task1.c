725cb03953591a568533620e9445ba490144c10f
unknown <ITMM-230041@Q113.local>
2023-09-29 14:32:07 +0300

    Update -a

--- Task1Solution/Task1/Task1.c
+++ Task1Solution/Task1/Task1.c
@@ -9,6 +9,7 @@ int main()
     scanf(" %lf, %lf, %lf", &x1, &y1, &r1);
     if (r1 <= 0)
     {
+        printf("radius 1st circle is incorrect");
         return 0;
     }
     printf("x=%lf, y=%lf, r=%lf \n", x1, y1, r1);
@@ -16,62 +17,38 @@ int main()
     scanf(" %lf, %lf, %lf", &x2, &y2, &r2);
     if (r2 <= 0)
     {
+        printf("radius 2nd circle is incorrect");
         return 0;
     }
     printf("x=%lf, y=%lf, r=%lf \n", x2, y2, r2);
-    if ((x1 == x2) && (y1 == y2))
+    if ((x1 == x2) && (y1 == y2) && (r1 == r2))
+    { 
+        printf("Circle match");
+        return 0;
+    }
+    double distance, a,b;
+    a = x1 - x2;
+    b = y1 - y2;
+    distance = sqrt(a * a + b * b);
+    printf("Distance=%lf \n", distance);
+    if (distance > r1 + r2)
     {
-        if (r1 == r2)
-        {
-            printf("Circle match");
-        }
-        else if ((r1 < r2) || (r2 < r1))
-        {
-            printf("Circle in circle");
-        }
+        printf("Circles have not general dots");
+    }
+    else if (distance == r1 + r2)
+    {
+        printf("Circles have one general dots");
+    }
+    else if (distance == abs(r2 - r1))
+    {
+        printf("Circles have one general dots and circle in circle");
+    }
+    else if (distance < abs(r2 - r1))
+    {
+        printf("Circle in circle");
     }
     else
     {
-        double distance, a,b;
-        if (x1 <= x2)
-        {
-            a = x2 - x1;
-        }
-        else if (x1 > x2)
-        {
-            a= x1 - x2;
-
-        }
-        if (y1<=y2)
-        {
-            b = y2 - y1;
-        }
-        else if (y1 > y2)
-        {
-            b = y1 - y2;
-        }
-        distance = sqrt(a * a + b * b);
-        printf("Distance=%lf \n", distance);
-        if (distance > r1 + r2)
-        {
-            printf("Circles have not general dots");
-        }
-        else if (distance == r1 + r2)
-        {
-            printf("Circles have one general dots");
-        }
-        else if (((distance < r1 + r2) && (r1 - r2 < distance)) || ((distance < r2 + r1) && (r2 - r1 < distance)))
-        {
-            printf("Circles have two general dots");
-        }
-        else if ((distance == r2 - r1) || (distance == r1 - r2))
-        {
-            printf("Circles have one general dots and circle in circle");
-        }
-        else if ((distance < r2 - r1) || (distance < r1 - r2))
-        {
-            printf("Circle in circle");
-        }
-
+        printf("Circles have two general dots");
     }
 }
