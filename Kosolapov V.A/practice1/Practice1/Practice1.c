49afa7991e1156eb5550fd4be3718a8cfa0e352c
Kosolapov-Vitaliy <ITMM-230041@Q113.local>
2023-10-13 13:35:52 +0300

    Class Update

--- Kosolapov V.A/practice1/Practice1/Practice1.c
+++ Kosolapov V.A/practice1/Practice1/Practice1.c
@@ -2,13 +2,8 @@
 
 int main()
 {
-    double h, w, d, m, m1,m2,m3,m4,m5,i;
+    double h, w, d, m_closet, m_wardrobe,m_sidewalls,m_covers,m_doors,m_shelfs,i;
     double k = 0;
-    do
-    {
-        printf("Input width:");
-        scanf("%lf", &w);
-    } while ((w < 80) || (w > 120));
     do
     {
         printf("Input height:");
@@ -16,21 +11,27 @@ int main()
     } while ((h < 180) || (h > 220));
     do
     {
+        printf("Input width:");
+        scanf("%lf", &w);
+    } while ((w < 80) || (w > 120));
+    do
+    {
         printf("Input depth:");
         scanf("%lf", &d);
     } while ((d < 50) || (d > 90));
-    double pDSP = 735, pDVP = 800, pTr = 670;
-    m1 = ((h * w * 0.5)/1000000)*pDVP;
-    m2 = (((h * d * 1.5)*2) / 1000000) * pDSP;
-    m3 = (((d * w * 1.5)*2) / 1000000) * pDSP;
-    m4 = ((h * w * 1) / 1000000) * pTr;
+    double pDSP = 650, pDVP = 950, pTr = 660;
+    m_wardrobe = ((h * w * 0.5)/1000000)*pDVP;
+    m_sidewalls = (((h * d * 1.5)*2) / 1000000) * pDSP;
+    m_covers = (((d * w * 1.5)*2) / 1000000) * pDSP;
+    m_doors = ((h * w * 1) / 1000000) * pTr;
     i = h;
     while (i >= 40)
     {
         k++;
         i = i - (40 + 1.5);
     }
-    m5 = ((k * ((w - 3) * d * 1.5)) / 1000000) * pDSP;
-    m = m1 + m2 + m3 + m4 + m5;
-    printf("m = %lf", m);
+    m_shelfs = ((k * ((w - 3) * d * 1.5)) / 1000000) * pDSP;
+    m_closet = m_wardrobe + m_sidewalls + m_covers + m_doors + m_shelfs;
+    printf("m = %lf", m_closet);
+    return 0;
 }
