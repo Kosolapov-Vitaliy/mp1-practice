f630d748e88fc7da22e6f5502cf39245ac0d0043
Kosolapov-Vitaliy <ITMM-230041@Q113.local>
2023-11-17 14:19:54 +0300

    Update

---
+++ KosolapovVA/Practice4/Practice4/Practice4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define name {"Milk","Bread","Sausage","Cheese", "Flakes", "Fish fillet", "Pasta", "Sugar", "Juice", "Chocolate"}
+#define price {99.9, 25.9, 189.9, 399.9, 199.9, 349.9, 139.9, 95.9, 119.9, 99.9}
+#define barcode {{1,0,3,1},{2,0,4,1}, {3,0,5,1},{1,1,6,1},{4,0,7,1},{3,1,8,1},{2,1,9,1},{5,0,1,2},{6,0,2,2},{7,0,3,2}}
+#define discount [10,5,20,3,1,8,25,4,50,30]
+
+int main()
+{
+    do {
+
+    } while();
+}
+
+
