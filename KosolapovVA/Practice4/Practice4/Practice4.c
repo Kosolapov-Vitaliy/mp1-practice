f6e6d8a8c754921a08eb0cd3d4b6817f3cf8d9e3
Kosolapov-Vitaliy <givit3324@gmail.com>
2023-12-14 00:42:54 +0300

    Update

--- KosolapovVA/Practice4/Practice4/Practice4.c
+++ KosolapovVA/Practice4/Practice4/Practice4.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 #define m 4
 #define N 10
-char* name[N] = { "Milk","Bread","Sausage","Cheese", "Flakes", "Fish fillet", "Pasta", "Sugar", "Juice", "Chocolate" };
-double price[N] = { 99.9, 25.9, 189.9, 399.9, 199.9, 349.9, 139.9, 95.9, 119.9, 99.9 };
-int barcode[N][m] = { {1,0,3,1},{2,0,4,1}, {3,0,5,1},{1,1,6,1},{4,0,7,1},{3,1,8,1},{2,1,9,1},{5,0,1,2},{6,0,2,2},{7,0,3,2} };
-int discount[N] = {10, 5, 20, 3,1,8,25,4,50,30};
+
+char* name[N] = { "Milk","Bread","Sausage","Cheese", 
+"Flakes", "Fish fillet", "Pasta", "Sugar", "Juice", "Chocolate" };
+double price[N] = { 99.9, 25.9, 189.9, 399.9, 199.9, 
+349.9, 139.9, 95.9, 119.9, 99.9 };
+int barcode[N][m] = { {1,0,3,1},{2,0,4,1}, {3,0,5,1},{1,1,6,1},{4,0,7,1},
+    {3,1,8,1},{2,1,9,1},{5,0,1,2},{6,0,2,2},{7,0,3,2}};
+int discount[N] = {10, 5, 20, 3, 1, 8, 25, 4, 50, 30};
+char* Const_E1 = "Enter 1 if you want to scan the next item.\n";
+char* Const_E2 = "Enter 2 if you want to see the product description.\n";
+char* Const_E3 = "Enter 3 if you want to add product details to the receipt.\n";
+char* Const_E4 = "Enter 4 if you want to withdraw the receipt\n";
+char* Const_E5 = "Enter 5 if you want to withdraw the receipt\n";
+int operation = 1;
 
 void struct_craft(int i);
 int scan_barcode();
-void check_barcode(int u_bar, int *id);
+void check_barcode(int u_bar, int *f_id);
 void inf_output(int id);
+void sum_counting(int id);
 void print_receipt();
+double sum_receipt(int i);
+
 struct receipt
 {
     char* name;
@@ -20,18 +33,15 @@ struct receipt
     int discount;
     double sum;
 }pos[N];
+
 int main()
 {
-    int operation = 1, user_barcode, id, i = 0;
-    double sum = 0;
+    int user_barcode, id, i = 0;
     struct_craft(i);
-    print_receipt();
     do {
         switch (operation) 
         {
         case 1:
-            id = 10;
-            printf("Please scan the 4 digit barcode:");
             user_barcode = scan_barcode();
             check_barcode(user_barcode, &(id));
             if (id == 10)
@@ -40,33 +50,22 @@ int main()
             }
             else
             {
-                printf("Enter 1 if you want to scan the next item.\n Enter 2 if you want to see the product description.\n Enter 3 if you want to add product details to the receipt.\n Enter 4 if you want to withdraw the receipt\n Enter 5 if you want to withdraw the total amount.\n");
+                printf("%s%s%s%s%s", Const_E1, Const_E2, Const_E3, Const_E4, Const_E5);
                 scanf("%d", &operation);
             }
             break;
         case 2:
             inf_output(id);
-            printf("Enter 1 if you want to scan the next item.\n Enter 3 if you want to add product details to the receipt.\n Enter 4 if you want to withdraw the receipt\n Enter 5 if you want to withdraw the total amount.\n");
-            scanf("%d", &operation);
             break;
         case 3:
-            pos[id].count++;
-            pos[id].sum = (pos[id].price + pos[id].price * (pos[id].discount / 100)) * pos[id].count;
-            printf("Enter 1 if you want to scan the next item.\n Enter 4 if you want to withdraw the receipt\n Enter 5 if you want to withdraw the total amount.\n");
-            scanf("%d", &operation);
+            sum_counting(id);
             break;
         case 4:
             print_receipt();
-            printf("Enter 1 if you want to scan the next item.\n Enter 5 if you want to withdraw the total amount.\n");
-            scanf("%d", &operation);
             break;
         }
-    } while (operation != 5);
-    for (; i < N; i++)
-    {
-        sum = sum + pos[i].sum;
-    }
-    printf("Sum receipt = %g", sum);
+    } while (operation != 5);    
+    printf("Sum receipt = %g", sum_receipt(i));
     return 0;
 }
 void struct_craft(int i)
@@ -80,16 +79,17 @@ void struct_craft(int i)
         pos[i].sum = 0;
     }
 }
-void print_receipt()
+int scan_barcode()
 {
-    int i = 0;
-    for (; i < N; i++)
+    int u_bar;
+    printf("Please scan the 4 digit barcode:");
+    scanf("%d", &u_bar);
+    while (u_bar / 1000 >= 10)
     {
-        if (pos[i].count != 0)
-        {
-            printf("Name: %s,Price= %g, Count: %d, Discount= %d, Sum= %g \n", pos[i].name, pos[i].price, pos[i].count, pos[i].discount, pos[i].sum);
-        }
+        printf("Error, your barcode is too long, scan again:");
+        scanf("%d", &u_bar);
     }
+    return (u_bar);
 }
 void check_barcode(int u_bar, int *f_id)
 {
@@ -101,6 +101,7 @@ void check_barcode(int u_bar, int *f_id)
     }
     for (j = 0; j < N; j++)
     {
+        *f_id = 10;
         count = 0;
         for (i = 0; i < m; i++)
         {
@@ -112,22 +113,42 @@ void check_barcode(int u_bar, int *f_id)
         if (count == 4)
         {
             *f_id = j;
+            break;
         }
     }
 }
 void inf_output(int id)
 {
     printf("Name:%s, price:%lf, discount:%d \n", name[id], price[id], discount[id]);
+    printf("%s%s%s%s", Const_E1, Const_E3, Const_E4, Const_E5);
+    scanf("%d", &operation);
 }
-int scan_barcode()
+void sum_counting(int id)
 {
-    int u_bar;
-    scanf("%d", &u_bar);
-    while (u_bar / 1000 > 10)
-    {
-        printf("Error, your barcode is too long, scan again:");
-        scanf("%d", &u_bar);
-    }
-    return (u_bar);
+    pos[id].count++;
+    pos[id].sum = (pos[id].price - (pos[id].price * pos[id].discount / 100.0)) * pos[id].count;
+    printf("%s%s%s", Const_E1, Const_E4, Const_E5);
+    scanf("%d", &operation);
 }
-
+void print_receipt()
+{
+    int i = 0;
+    for (; i < N; i++)
+    {
+        if (pos[i].count != 0)
+        {
+            printf("Name:%s,Price=%g,", pos[i].name, pos[i].price);
+            printf("Count:%d, Discount=%d, Sum=%g\n", pos[i].count, pos[i].discount, pos[i].sum);
+        }
+    }
+    printf("%s%s", Const_E1, Const_E5);
+    scanf("%d", &operation);
+}
+double sum_receipt(int i)
+{
+    double sum = 0;
+    for (; i < N; i++)
+    {        
+        sum = (sum + pos[i].sum);
+    }
+}
